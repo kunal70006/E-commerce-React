32a970cace6d1dcd824fbc7786f6d2512505bc12
Taranpreet Singh <taranpreetsingh29@gmail.com>
2020-06-01 03:37:42 +0530

    Files made for login, gn

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import styles from './Register.module.css'
 
 
-const Register = () => {
+const Register = (props) => {
 
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
