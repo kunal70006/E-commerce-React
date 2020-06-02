5a4784ef0504c0c6f58b408a376123dd910f2f92
Kunal <singhkunaldeep@gmail.com>
2020-06-02 17:14:56 +0530

    working on shop, fixed register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,59 +1,57 @@
-import firebase from '../../Firebase'
-import { useState } from 'react'
-import styles from './Register.module.css'
-
+import React from "react";
+import firebase from "../../Firebase";
+import { useState } from "react";
+import styles from "./Register.module.css";
 
 const Register = (props) => {
-
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
   const registerButtonDidClick = (event) => {
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-  }
+    firebase.auth().createUserWithEmailAndPassword(email, password);
+  };
 
   return (
     <div>
-      <input 
-        type="email" 
-        placeholder="Enter your email" 
+      <input
+        type="email"
+        placeholder="Enter your email"
         value={email}
-        onChange={ (event) => {
-          setEmail(event.target.value)
+        onChange={(event) => {
+          setEmail(event.target.value);
         }}
       />
-      <input 
-        type="password" 
-        placeholder="Set your password" 
+      <input
+        type="password"
+        placeholder="Set your password"
         value={password}
-        onChange={ (event) => {
-          setPassword(event.target.value)
+        onChange={(event) => {
+          setPassword(event.target.value);
         }}
       />
-      <input 
-        type="password" 
-        placeholder="Confirm your password" 
+      <input
+        type="password"
+        placeholder="Confirm your password"
         value={confirmPassword}
-        onChange={ (event) => {
-          setConfirmPassword(event.target.value)
+        onChange={(event) => {
+          setConfirmPassword(event.target.value);
         }}
       />
 
       <label className={styles.passwordChecker}>
-        {(password === confirmPassword) ? '' : 'Passwords do not match'}
+        {password === confirmPassword ? "" : "Passwords do not match"}
       </label>
 
-      <div onClick={props.history.push('/login')}>
+      <div onClick={props.history.push("/login")}>
         Already have an account? Log in
       </div>
-      
+
       <div className={styles.registerButton} onClick={registerButtonDidClick}>
         Register
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 import styles from "./Shop.module.css";
 
 const Shop = () => {
   return (
     <div className={styles.Shop}>
-      <h1>In Shop</h1>
+      <Navbar />
+      <div className={styles.cardContainer}>
+        <div className={styles.title}>Antique Coins</div>
+        <div className={styles.card}>
+          <img src={require("./1.png")} alt="coin1" className={styles.image} />
+          <div className={styles.container}>
+            <h4>coin 1</h4>
+            <p className={styles.desc}>Lorem ipsum dolor sit amet.</p>
+            <p className={styles.price}>$69</p>
+            <button className={styles.toCartBtn}>Add to cart</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
